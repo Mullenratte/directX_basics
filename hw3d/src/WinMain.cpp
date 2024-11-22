9f0a003e696c4be19dfe4ea034c4b78287f5d66e
Mullenratte <Max.Blanc@web.de>
2024-11-22 11:25:02 +0100

    Create Message Loop and custom WndProc

--- hw3d/src/WinMain.cpp
+++ hw3d/src/WinMain.cpp
@@ -1,5 +1,18 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    // enables the application to quit properly when closing the window
+    // this is needed because DefWindowProc allows closing window but is not quitting the underlying application
+    case WM_CLOSE:
+        PostQuitMessage(10);
+        break;
+    }
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
 INT CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     const auto pClassName = L"DirectX Game";
@@ -7,7 +20,7 @@ INT CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     WNDCLASSEX wc = {};
     wc.cbSize = sizeof(wc);
     wc.style = CS_OWNDC;
-    wc.lpfnWndProc = DefWindowProc;
+    wc.lpfnWndProc = WndProc;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hInstance = hInstance;
@@ -34,8 +47,25 @@ INT CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
         hInstance,
         nullptr
     );
+
     ShowWindow(hWnd, SW_SHOW);
-    while(true);
+
+    // message pump
     
-    return 0;
+    // if wMsgFilterMin and -Max are both 0, GetMessage takes in ALL message numbers
+    MSG msg;
+    BOOL gResult;
+    while( (gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    if (gResult == -1)
+    {
+        return -1;
+    } else
+    {
+        return msg.wParam;
+    }
 }
